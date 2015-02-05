9eed59a8f4a955d062915fe937dc1c10b830fa9a
maksus01 <maksus01@hotmail.com>
2015-02-05 13:39:57 -0700

    finished most of parsing in main, have to fix start date

--- assign4/main.cpp
+++ assign4/main.cpp
@@ -17,16 +17,6 @@ using namespace boost::gregorian;
 using namespace std;
 
 
-bool contains(set<string> data, string to_find)
-{
-	BOOST_FOREACH(string s, data)
-	{
-		if( s == to_find)
-			return true;
-	}
-	return false;
-}
-
 
 int main(){
 	date current_day;
@@ -36,6 +26,7 @@ int main(){
 	map<string, warehouse> warehouses;
 	
 	set<string> heads;	
+
 	
 	string headers [7] = {"FoodItem", "Warehouse", "Start date", "Receive", "Request", "Next day", "End"};
 	
@@ -56,6 +47,7 @@ int main(){
 		
 		if(!word.compare(0,headers[0].size(), headers[0]))
 		{
+			//Food Item
 			for(vector<string>::iterator it = word_array.begin(); it != word_array.end(); it++)
 			{
 				string name, code, shelf_life;
@@ -84,80 +76,97 @@ int main(){
 				}
 			}
 		}
-		/*
 		else if(!word.compare(0,headers[1].size(), headers[1]))
-		{	
+		{	//Warehouse
+			string warehouse_name = "";
 			for(vector<string>::iterator it = word_array.begin(); it != word_array.end(); it++)
 			{
-				string _warehouse = "";
-				if(!(*it).compare("Warehouse") || !(*it).compare("-") || !(*it).compare(" "))
+				if(!(*it).compare("Warehouse") || !(*it).compare("-") || !(*it).compare(""))
 					continue;
 				else
 				{
 					while(it != word_array.end())
 					{
-						warehouse += *it;
+						warehouse_name += *it;
 						it++;
 					}
-					warehouse new_warehouse (_warehouse);
-					warehouses[_warehouse] = new_warehouse;
+					warehouse new_warehouse (warehouse_name);
+					warehouses[warehouse_name] = new_warehouse;
 					break;
 				}
 				
 			}
 		}
-		*/
-		/*
 		else if(!word.compare(0,headers[2].size(), headers[2]))
-		{
-			//Start date
+		{	
+			//Start Date
+			string start_date = "";
+			for(vector<string>::iterator it = word_array.begin(); it != word_array.end(); it++)
+			{
+				if(!(*it).compare("date:"))
+				{
+					it++;
+					start_date = *it;
+					break;
+				}	
+			}
+			
+
+			vector<string> date_parts;
+			
+			boost::split(date_parts, start_date, boost::is_any_of("/"));
+			string temp_date = date_parts[2];
+			temp_date += "/" + date_parts[0] + "/" + date_parts[1];
+			
+			current_day = from_string("2010/01/05");
+			
+			cout << to_simple_string(current_day) << endl;
+			
+			//cout << temp_date << endl;
+			
 			
 		}
 		else if(!word.compare(0,headers[3].size(), headers[3]))
 		{
 			//Receive
-			cout << headers[3] << word <<  endl;
+			string code = word_array[0];
+			int quantity = atoi(word_array[1].c_str());
+			string warehouse = word_array[2];
+			
+			warehouses[warehouse].receive(food_items[code], current_day, quantity);
+
 		}
 		else if(!word.compare(0,headers[4].size(), headers[4]))
 		{
 			//Request
-			cout << headers[4] << word <<  endl;
+			string code = word_array[0];
+			int quantity = atoi(word_array[1].c_str());
+			string warehouse = word_array[2];
+			warehouses[warehouse].request(food_items[code], quantity);
 		}
 		else if(!word.compare(0,headers[5].size(), headers[5]))
 		{
-			//Next day
-			cout << headers[5] << word <<  endl;
+			//Next Day
+			date_duration dd(1);
+			current_day += dd;
 		}
 		else if(!word.compare(0,headers[6].size(), headers[6]))
 		{
-			//End
-			cout << headers[6] << word << endl;
+			break;
+			
 		}	
 		else
 		{
 			cout << "WRONG COMMAND" << endl;
 		}
-		
-		
-		if(contains(heads, word))
-		{
-			cout << word << " -> " << "AYY" << endl;
-		}
-		else
-		{
-			cout << word << " ! " << "lmao"<< endl;
-		}
-		*/
+
 		if (in.fail())
 			break;
 			
 	}
-	//cout << "############################################" << endl;
-	for(vector<string>::iterator it = arr.begin(); it != arr.end(); it++)
-	{
-		string s = *it;
-		//cout << s << endl;
-	}
+
+	
+
 		
 	cout << "End of main" << endl;
 	
